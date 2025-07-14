713ea367800a71c94fd65c543d9848c57ae6469e
taktarovgv <taktarovgv@gmail.com>
2025-07-14 11:44:53 +0700

    Исправляем ошибки с постоянным редиректором уже в приложении_12

--- src/components/providers/index.ts
+++ src/components/providers/index.ts
@@ -1,6 +1,6 @@
 // src/components/providers/index.ts
-export { TelegramProvider, useTelegram, TelegramStatus } from './TelegramProvider';
-// ✅ ИСПРАВЛЕНО: TelegramStatus теперь экспортируется из TelegramProvider вместо отдельного файла
+export { TelegramProvider, useTelegram, TelegramDebugStatus } from './TelegramProvider';
+// ✅ ИСПРАВЛЕНО: Экспортируем TelegramDebugStatus вместо несуществующего TelegramStatus
 export { useTelegramEnvironment, useTelegramAuth } from '../../hooks/useTelegramHooks';
 
 // Экспорт типов для удобства
